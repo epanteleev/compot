ad4070633429f17d4f8420e14f36a273b1ab352f
epanteleev <msl.epanteleev@gmail.com>
2023-10-08 13:40:59 +0300

    improve test-runner

--- tests/test-runner.sh
+++ tests/test-runner.sh
@@ -1,5 +1,9 @@
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+RESET=`tput sgr0`
+
 JAVA=java
-IR_COMPILER="${JAVA} -jar ../ssa-ir/target/ssa-ir-1.0-SNAPSHOT-jar-with-dependencies.jar"
+IR_COMPILER="${JAVA} -ea -jar ../ssa-ir/target/ssa-ir-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
 function compile_test() {
 	${IR_COMPILER} "$1.ir"
@@ -8,33 +12,53 @@ function compile_test() {
 }
 
 function run_test() {
-	echo "[Run base: $1"]
-	./$1/base
+	echo "${GREEN}[Run base: $1]${RESET}"
+	BASE_RESULT=$(./$1/base)
+	check $1 "$2" "$BASE_RESULT"
+
 	echo "[Run opt: $1]"
-	./$1/opt
+	OPT_RESULT=$(./$1/opt)
+	check $1 "$2" "$OPT_RESULT"
 }
 
+function check() {
+	result=$3
+	expected=$2
+	if [ "$result" == "$expected" ];
+	then
+  		echo -e "\t${GREEN}[SUCCESS]${RESET}"
+	else
+  		echo -e "\t${RED}[FAIL]: '$1'${RESET}"
+  		echo -e "\t${GREEN}[Expected]: '$expected'${RESET}"
+  		echo -e "\t${RED}[Actual]:${RESET} '$result'"
+	fi
+}
+
+
 compile_test manyArguments
-run_test manyArguments
+run_test manyArguments 36
 
 compile_test fib
-run_test fib
+run_test fib 21
 
 compile_test fib_recursive
-run_test fib_recursive
+run_test fib_recursive 21
 
 compile_test discriminant
-run_test discriminant
+run_test discriminant -192
 
 compile_test factorial
-run_test factorial
+run_test factorial 40320
 
 compile_test manyBranches
-run_test manyBranches
+run_test manyBranches "7
+0"
 
 compile_test clamp
-run_test clamp
+run_test clamp "9
+10
+8"
 
 compile_test fill_in_array0
-run_test fill_in_array0
+run_test fill_in_array0 "12345"
 
