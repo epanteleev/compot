b5b03634d2a7d11dd64428690deaff9d80509c4c
epanteleev <msl.epanteleev@gmail.com>
2024-03-25 15:23:53 +0200

    more work

--- ssa/src/main/kotlin/ir/platform/x64/regalloc/liveness/LiveRange.kt
+++ ssa/src/main/kotlin/ir/platform/x64/regalloc/liveness/LiveRange.kt
@@ -25,55 +25,11 @@ open class LiveRange internal constructor(protected val creation: OrderedLocatio
 }
 
 class LiveRangeImpl internal constructor(creation: OrderedLocation, end: OrderedLocation): LiveRange(creation, end) {
-    private fun addWork(worklist: MutableList<Block>, visited: MutableSet<Block>, bb: Block) {
-        if (bb == creation.block) {
-            return
-        }
-
-        for (p in bb.predecessors()) {
-            if (visited.contains(p)) {
-                continue
-            }
-            worklist.add(p)
-        }
-    }
-
     internal fun registerUsage(location: OrderedLocation, bbOrdering: Map<Block, Int>) {
-        val worklist = arrayListOf<Block>() //TODO
-        val visited  = mutableSetOf<Block>()
-        addWork(worklist, visited, location.block)
-
-        var currentKillerBlock = location.block
-
-        while (worklist.isNotEmpty()) {
-            val bb = worklist.removeLast()
-            if (visited.contains(bb)) {
-                continue
-            }
-
-            val ord = bbOrdering[bb]!!
-            if (ord > bbOrdering[currentKillerBlock]!!) {
-                currentKillerBlock = bb
-                break
-            }
-
-            visited.add(bb)
-            addWork(worklist, visited, bb)
-        }
-
-        worklist.clear()
-        visited.clear()
-
-        end = if (currentKillerBlock == location.block) {
-            if (end >= location) {
-                end
-            } else {
-                location
-            }
+        end = if (end >= location) {
+            end
         } else {
-            OrderedLocation(currentKillerBlock,
-                currentKillerBlock.instructions().size,
-                bbOrdering[currentKillerBlock]!!)
+            location
         }
     }
 }
