4a5b68fde120b5e5c568bcf890a68d2700ddbb03
epanteleev <msl.epanteleev@gmail.com>
2024-05-02 19:16:04 +0300

    clanup code in c compiler driver

--- shlang/src/main/kotlin/startup/ShlangDriver.kt
+++ shlang/src/main/kotlin/startup/ShlangDriver.kt
@@ -1,24 +1,34 @@
 package startup
 
 import gen.IRGen
-import parser.CProgramParser
-import preprocess.CProgramPreprocessor
-import preprocess.FileHeaderHolder
-import preprocess.HeaderHolder
-import preprocess.PreprocessorContext
-import tokenizer.CTokenizer
+import preprocess.*
 import java.io.File
+import ir.module.Module
+import tokenizer.CTokenizer
+import parser.CProgramParser
+
 
 class ShlangDriver(private val cli: CCLIArguments) {
-    fun run() {
+
+    private fun initializePreprocessorContext(): PreprocessorContext {
+        val headerHolder = FileHeaderHolder(setOf(File(cli.getFilename()).parent))
+        return PreprocessorContext.empty(headerHolder)
+    }
+
+    private fun compile(): Module {
         val source = File(cli.getFilename()).readText()
-        val ctx = PreprocessorContext.empty(FileHeaderHolder(setOf(File(cli.getFilename()).parent)))
-        val tokens = CTokenizer.apply(source)
-        val preprocessor = CProgramPreprocessor.create(tokens, ctx)
+        val ctx    = initializePreprocessorContext()
+
+        val tokens              = CTokenizer.apply(source)
+        val preprocessor        = CProgramPreprocessor.create(tokens, ctx)
         val postProcessedTokens = preprocessor.preprocessWithKilledSpaces()
 
         val program = CProgramParser.build(postProcessedTokens).translation_unit()
-        val module = IRGen.apply(program)
+        return IRGen.apply(program)
+    }
+
+    fun run() {
+        val module = compile()
         OptDriver(cli.makeOptCLIArguments()).run(module)
     }
 }
