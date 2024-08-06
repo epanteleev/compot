3c8c0eb3e5592acc09f697da180f08f532d196da
epanteleev <msl.epanteleev@gmail.com>
2024-08-06 21:11:05 +0300

    Introduce accurate live intervals

--- opt/src/commonMain/kotlin/ir/pass/analysis/intervals/LiveRange.kt
+++ opt/src/commonMain/kotlin/ir/pass/analysis/intervals/LiveRange.kt
@@ -1,29 +1,38 @@
 package ir.pass.analysis.intervals
 
+import ir.module.block.Block
 import ir.utils.OrderedLocation
 
 
-abstract class LiveRange internal constructor(private val creation: OrderedLocation, protected var end: OrderedLocation) {
+abstract class LiveRange internal constructor(private var creation: OrderedLocation, protected var locations: MutableMap<Block, OrderedLocation>) {
     fun begin(): OrderedLocation = creation
-    fun end(): OrderedLocation = end
+    fun end(): OrderedLocation = locations.maxBy { it.value.order }.value //TODO cached
 
     fun merge(other: LiveRange): LiveRange {
-        val begin = minOf(other.creation, creation, compareBy { it.order })
-        val end = maxOf(other.end, end, compareBy { it.order })
-        return LiveRangeImpl(begin, end)
+        if (creation > other.creation) {
+            creation = other.creation
+        }
+
+        for ((block, location) in other.locations) {
+            val loc = locations[block]
+            if (loc == null || loc < location) {
+                locations[block] = location
+            }
+        }
+
+        return this
     }
 
     override fun toString(): String {
-        return "range [$creation : $end]"
+        return "range [$creation : ${end()}]"
     }
 }
 
-class LiveRangeImpl internal constructor(creation: OrderedLocation, end: OrderedLocation): LiveRange(creation, end) {
+class LiveRangeImpl internal constructor(creation: OrderedLocation, end: OrderedLocation): LiveRange(creation, hashMapOf(Pair(creation.block, end))) {
     internal fun registerUsage(location: OrderedLocation) {
-        if (end >= location) {
-            return
+        val loc = locations[location.block]
+        if (loc == null || loc < location) {
+            locations[location.block] = location
         }
-
-        end = location
     }
 }
