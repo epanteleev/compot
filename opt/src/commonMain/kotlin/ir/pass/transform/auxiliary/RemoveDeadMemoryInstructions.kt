0c873143003d2cec4ecdd46d2a015b3c03f3e378
epanteleev <msl.epanteleev@gmail.com>
2024-08-28 10:12:37 +0300

    Refactor

--- opt/src/commonMain/kotlin/ir/pass/transform/auxiliary/RemoveDeadMemoryInstructions.kt
+++ opt/src/commonMain/kotlin/ir/pass/transform/auxiliary/RemoveDeadMemoryInstructions.kt
@@ -4,13 +4,14 @@ import ir.module.*
 import ir.instruction.*
 import ir.module.block.Block
 import ir.pass.isLocalVariable
+import ir.types.PrimitiveType
 
 
 class RemoveDeadMemoryInstructions private constructor(private val cfg: BasicBlocks) {
     private fun removeMemoryInstructions(bb: Block) {
         fun filter(instruction: Instruction): Boolean {
             return when (instruction) {
-                is Alloc -> instruction.isLocalVariable()
+                is Alloc -> instruction.allocatedType is PrimitiveType && instruction.isLocalVariable()
                 is Store -> instruction.isLocalVariable()
                 is Load  -> instruction.isLocalVariable()
                 else -> false
