cb925a0f71aea3b85fcf0ef139afafbd5bcbfe02
epanteleev <msl.epanteleev@gmail.com>
2024-08-28 10:12:36 +0300

    Modify test

--- testcases/src/resources/shlang/initializerList/initializerList5.c
+++ testcases/src/resources/shlang/initializerList/initializerList5.c
@@ -3,10 +3,10 @@ extern void printf(char format[], ...);
 
 int main() {
     int bb = 3;
-    int a[][3] = {{1, bb, 3}, {4, 5, 6}, {7, 8, 9}};
+    int a[][3] = {{1, bb, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
 
     for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < 4; j++)
             printf("%d ", a[i][j]);
         printf("\n");
     }
