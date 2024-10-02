1b2d38f2e49ed41101f7fb19fdfd03d48df5da85
epanteleev <msl.epanteleev@gmail.com>
2024-10-02 10:22:56 +0300

    Accurate live intervals 2

--- opt/src/commonMain/kotlin/ir/pass/analysis/intervals/LiveRange.kt
+++ opt/src/commonMain/kotlin/ir/pass/analysis/intervals/LiveRange.kt
@@ -10,6 +10,26 @@ abstract class LiveRange internal constructor(protected var locations: MutableMa
 
     fun intersect(other: LiveRange): Boolean {
         return !(begin().from > other.end().to || other.begin().from > end().to) //TODo not worried about holes
+        /*val maxRange = if (locations.size >= other.locations.size) {
+            locations
+        } else {
+            other.locations
+        }
+
+        val minRange = if (locations.size >= other.locations.size) {
+            other.locations
+        } else {
+            locations
+        }
+
+        for ((block, location) in maxRange) {
+            val otherLocation = minRange[block] ?: continue
+            if (location.intersect(otherLocation)) {
+                return true
+            }
+        }
+
+        return false*/
     }
 
     override fun toString(): String {
@@ -30,25 +50,33 @@ class LiveRangeImpl internal constructor(val creationBlock: Block, creation: Loc
     }
 
     private fun propagateLocation(bb: Block, domTree: DominatorTree, locationMap: Map<Block, Int>) {
+        if (bb == creationBlock) {
+            val start = locationMap[bb]!!
+            val loc = locations[bb]!!
+            locations[bb] = loc.mergeTo(start + bb.size)
+            return
+        }
+
         for (dom in domTree.dominators(bb)) {
-            if (creationBlock == dom) {
-                return
-            }
+            dom as Block
             val start = locationMap[dom]!!
-            locations[dom as Block] = Location(start, start + dom.size)
+            val loc = locations[dom]
+            if (loc != null) {
+                locations[dom] = loc.mergeTo(start + dom.size)
+                return
+            } else {
+                locations[dom] = Location(start, start + dom.size)
+            }
         }
     }
 
     internal fun registerUsage(bb: Block, domTree: DominatorTree, locationMap: Map<Block, Int>, location: Int) {
         val loc = locations[bb]
         if (loc == null) {
-            locations[bb] = Location(location, location)
+            locations[bb] = Location(locationMap[bb]!!, location)
+            propagateLocation(bb, domTree, locationMap)
         } else {
             locations[bb] = loc.mergeTo(location)
         }
-        if (bb == creationBlock) {
-            return
-        }
-        propagateLocation(bb, domTree, locationMap)
     }
 }
