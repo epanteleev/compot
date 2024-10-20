d4db7d1031305645fa3213ccecd49f106fabd4be
epanteleev <msl.epanteleev@gmail.com>
2024-10-20 16:46:40 +0300

    Add tests

--- shlang-driver/src/commonTest/kotlin/shlang/LACCTests.kt
+++ shlang-driver/src/commonTest/kotlin/shlang/LACCTests.kt
@@ -96,6 +96,104 @@ abstract class LACCTests : CommonCTest() {
         assertEquals("-1\n", result.output)
         assertEquals(0, result.exitCode)
     }
+
+    @Test
+    fun testBitwiseComplement() {
+        val result = runCTest("shlang/lacc/bitwise-complement", listOf(), options())
+        assertEquals(228, result.exitCode)
+    }
+
+    @Test
+    fun testBitwiseConstant() {
+        val result = runCTest("shlang/lacc/bitwise-constant", listOf(), options())
+        assertEquals(79, result.exitCode)
+    }
+
+    @Test
+    fun testBitwiseExpression() {
+        val result = runCTest("shlang/lacc/bitwise-expression", listOf(), options())
+        assertEquals(0, result.exitCode)
+    }
+
+    @Test
+    @Ignore
+    fun testBitwiseSignExtend() {
+        val result = runCTest("shlang/lacc/bitwise-sign-extend", listOf(), options())
+        val expect = "-7116886904016191872, 7116886904016191871, -1\n"
+        assertEquals(expect, result.output)
+        assertEquals(46, result.exitCode)
+    }
+
+    @Test
+    fun testByteLoad() {
+        val result = runCTest("shlang/lacc/byte-load", listOf(), options())
+        assertEquals(101, result.exitCode)
+    }
+
+    @Test
+    @Ignore
+    fun testCastFloatUnion() {
+        val result = runCTest("shlang/lacc/cast-float-union", listOf(), options())
+        assertEquals("549754765312.000000\n", result.output)
+        assertEquals(20, result.exitCode)
+    }
+
+    @Test
+    fun testCastFloat() {
+        val result = runCTest("shlang/lacc/cast-float", listOf(), options())
+        assertEquals(1, result.exitCode)
+    }
+
+    @Test
+    fun testCastFunctionArgs() {
+        val result = runCTest("shlang/lacc/cast-function-args", listOf(), options())
+        assertEquals("18446744073709551613, 253, 2.710000, 3.140000\n", result.output)
+        assertEquals(46, result.exitCode)
+    }
+
+    @Test
+    fun testCastFunction() {
+        val result = runCTest("shlang/lacc/cast-function", listOf(), options())
+        assertEquals("foo\n", result.output)
+        assertEquals(0, result.exitCode)
+    }
+
+    @Test
+    fun testCastImmediateTruncate() {
+        val result = runCTest("shlang/lacc/cast-immediate-truncate", listOf(), options())
+        assertEquals("44480, 254, -34\n", result.output)
+        assertEquals(16, result.exitCode)
+    }
+
+    @Test
+    @Ignore
+    fun testCast() {
+        val result = runCTest("shlang/lacc/cast", listOf(), options())
+        assertEquals(1, result.exitCode)
+    }
+
+    @Test
+    fun testCommaSideEffects() {
+        val result = runCTest("shlang/lacc/comma-side-effects", listOf(), options())
+        assertEquals("453128631, 453128631\n", result.output)
+        assertEquals(21, result.exitCode)
+    }
+
+    @Test
+    @Ignore
+    fun testComment() {
+        val result = runCTest("shlang/lacc/comment", listOf(), options())
+        assertEquals("24, 24, w ??=*/ /*t???, a\"/*b\"\\\\, 34, 39\n", result.output)
+        assertEquals(41, result.exitCode)
+    }
+
+    @Test
+    @Ignore
+    fun testCompare() {
+        val result = runCTest("shlang/lacc/compare", listOf(), options())
+        assertEquals("1, 0, 0, 1\n", result.output)
+        assertEquals(11, result.exitCode)
+    }
 }
 
 class LACCTestsO0: LACCTests() {
