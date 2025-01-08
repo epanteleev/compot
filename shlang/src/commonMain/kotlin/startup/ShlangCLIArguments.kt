9cd469a1e43e775a571d83c2c4187d7d08589069
epanteleev <msl.epanteleev@gmail.com>
2025-01-08 11:54:04 +0200

    More work

--- shlang/src/commonMain/kotlin/startup/ShlangCLIArguments.kt
+++ shlang/src/commonMain/kotlin/startup/ShlangCLIArguments.kt
@@ -1,16 +1,37 @@
 package startup
 
 import common.Files
-import common.commandLine.AnyCLIArguments
 
 
-class ShlangCLIArguments : AnyCLIArguments() {
+class ShlangCLIArguments {
     private val includeDirectories = hashSetOf<String>()
     private val defines = hashMapOf<String, String>()
     private var preprocessOnly = false
     private var dumpDefines = false
     private var optionC = false
     private var inputFilename = "<input>"
+
+    private var dumpIrDirectoryOutput: String? = null
+    private var optimizationLevel = 0
+    private var outFilename: String? = null
+
+    fun isDumpIr(): Boolean = dumpIrDirectoryOutput != null
+
+    fun setDumpIrDirectory(out: String) {
+        dumpIrDirectoryOutput = out
+    }
+
+    fun setOutputFilename(name: String) {
+        outFilename = name
+    }
+
+    fun setOptLevel(level: Int) {
+        optimizationLevel = level
+    }
+
+    protected fun getName(name: String): String {
+        return Files.getBasename(name)
+    }
 
     fun getOutputFilename(): String {
         if (outFilename != null) {
